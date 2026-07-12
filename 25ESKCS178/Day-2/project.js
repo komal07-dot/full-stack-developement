2d50793f8c36054b4251ff0223d1f842f2706809
komal07-dot <komalpihipriya@gmail.com>
2026-07-12 23:11:11 +0530

    Update project.js

--- 25ESKCS178/Day-2/project.js
+++ 25ESKCS178/Day-2/project.js
@@ -1,1 +1,11 @@
+const darkModeBtn = document.getElementById("darkModeBtn");
 
+darkModeBtn.addEventListener("click", function () {
+    document.body.classList.toggle("dark");
+
+    if (document.body.classList.contains("dark")) {
+        darkModeBtn.innerHTML = "☀️ Light Mode";
+    } else {
+        darkModeBtn.innerHTML = "🌙 Dark Mode";
+    }
+});
