6ac81f674b350e3d0e7667769aed4f83a05e96c3
komal07-dot <komalpihipriya@gmail.com>
2026-07-12 23:49:32 +0530

    Update script.js

--- 25ESKCS178/Day5/script.js
+++ 25ESKCS178/Day5/script.js
@@ -1,1 +1,17 @@
+let count=0;
 
+function increase(){
+
+count++;
+
+document.getElementById("count").innerHTML=count;
+
+}
+
+function decrease(){
+
+count--;
+
+document.getElementById("count").innerHTML=count;
+
+}
