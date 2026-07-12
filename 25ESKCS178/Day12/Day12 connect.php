9ae8d8c1fff43f86af1d17be75f29b9a69635d03
komal07-dot <komalpihipriya@gmail.com>
2026-07-13 00:02:41 +0530

    Update Day12 connect.php

--- 25ESKCS178/Day12/Day12 connect.php
+++ 25ESKCS178/Day12/Day12 connect.php
@@ -1,1 +1,37 @@
+<?php
 
+$conn=new mysqli("localhost","root","","feedback_db");
+
+if($conn->connect_error){
+
+die("Connection Failed");
+
+}
+
+if($_SERVER["REQUEST_METHOD"]=="POST"){
+
+$name=$_POST["name"];
+
+$email=$_POST["email"];
+
+$course=$_POST["course"];
+
+$rating=$_POST["rating"];
+
+$feedback=$_POST["feedback"];
+
+$stmt=$conn->prepare("INSERT INTO feedback(name,email,course,rating,feedback) VALUES(?,?,?,?,?)");
+
+$stmt->bind_param("sssis",$name,$email,$course,$rating,$feedback);
+
+$stmt->execute();
+
+echo "<h2>Feedback Submitted Successfully</h2>";
+
+$stmt->close();
+
+$conn->close();
+
+}
+
+?>
