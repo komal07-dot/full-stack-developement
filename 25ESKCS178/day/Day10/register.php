ae370143c79a57b8de3290a10f16d35e1288a4b7
komal07-dot <komalpihipriya@gmail.com>
2026-07-12 23:56:59 +0530

    Update register.php

--- 25ESKCS178/day/Day10/register.php
+++ 25ESKCS178/day/Day10/register.php
@@ -1,1 +1,19 @@
+<?php
 
+if($_SERVER["REQUEST_METHOD"]=="POST"){
+
+$name=$_POST["name"];
+$email=$_POST["email"];
+$password=$_POST["password"];
+
+echo "<h2>Registration Successful</h2>";
+
+echo "Name : ".$name."<br>";
+
+echo "Email : ".$email."<br>";
+
+echo "Password : ".$password;
+
+}
+
+?>
