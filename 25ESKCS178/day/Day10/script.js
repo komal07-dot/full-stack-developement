6f89be1ad27488d3bdb63f9182839c99eeb44f3d
komal07-dot <komalpihipriya@gmail.com>
2026-07-12 23:56:08 +0530

    Update script.js

--- 25ESKCS178/day/Day10/script.js
+++ 25ESKCS178/day/Day10/script.js
@@ -1,1 +1,19 @@
+function validateForm(){
 
+let name=document.getElementById("name").value;
+let email=document.getElementById("email").value;
+let password=document.getElementById("password").value;
+
+if(name=="" || email=="" || password==""){
+alert("Please fill all fields");
+return false;
+}
+
+if(password.length<6){
+alert("Password must be at least 6 characters");
+return false;
+}
+
+return true;
+
+}
